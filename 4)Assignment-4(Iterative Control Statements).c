1a2e1403a771b25803a37946e8c8360ee01f5d52
sebubenchuser <109805632+sebubenchuser@users.noreply.github.com>
2022-08-12 23:30:37 +0530

    Add files via upload

---
+++ 4)Assignment-4(Iterative Control Statements).c
@@ -0,0 +1,145 @@
+                                 //Assignment - 4 A (Iterative Control Statements) (completed)
+
+//1. Write a program to print MySirG 5 times on the screen
+
+     /*#include<stdio.h>
+     int main()
+     {
+         int i;
+         for(i=0;i<5;i++)
+         {
+         printf("MysirG\n");
+         }
+     }*/
+
+
+//2. Write a program to print the first 10 natural numbers.
+
+   /*#include<stdio.h>
+   int main()
+   {
+       int i;
+       for(i=1;i<=10;i++)
+       {
+           printf("%d ",i);
+       }
+       return 0;
+   }*/
+
+//3. Write a program to print the first 10 natural numbers in reverse order
+
+    /*#include<stdio.h>
+    int main()
+    {
+        int i;
+        for(i=10;i>=1;i--)
+        {
+            printf("%d ",i);
+        }
+        return 0;
+    }*/
+
+//4. Write a program to print the first 10 odd natural numbers
+
+     /*#include<stdio.h>
+     int main()
+     {
+         int i,c=1;
+         for(i=1;c<=10;i++)
+         {
+            if(i%2!=0)
+            {
+            printf("%d ",i);
+            c++;
+            }
+         }
+         return 0;
+     }*/
+
+//5. Write a program to print the first 10 odd natural numbers in reverse order.
+
+   /*#include<stdio.h>
+   int main()
+   {
+       int i,c=1;
+       for(i=20;c<=10;i--)
+       {
+          if(i%2!=0)
+          {
+              printf("%d ",i);
+              c++;
+          }
+       }
+
+       return 0;
+   }*/
+
+//6. Write a program to print the first 10 even natural numbers
+
+ /* #include<stdio.h>
+  int main()
+  {
+      int i=0,c=1;
+      while(c<=10)
+      {
+          i=i+2;
+          printf("%d ",i);
+          c++;
+      }
+      return 0;
+  }*/
+
+//7. Write a program to print the first 10 even natural numbers in reverse order
+
+   /*#include<stdio.h>
+   int main()
+   {
+       int i,c=1;
+       for(i=20;c<=10;i--)
+       {
+           if(i%2==0)
+           {
+               printf("%d ",i);
+               c++;
+           }
+       }
+   }*/
+
+//8. Write a program to print squares of the first 10 natural numbers
+
+   /*#include<stdio.h>
+   int main()
+   {
+       int i=1;
+       for(i=1;i<=10;i++)
+       {
+           printf("%d ",i*i);
+       }
+       return 0;
+   }*/
+
+//9. Write a program to print cubes of the first 10 natural numbers
+
+ /*#include<stdio.h>
+ int main()
+ {
+     int i;
+     for(i=1;i<=10;i++)
+     {
+         printf("%d ",i*i*i);
+     }
+ }*/
+
+//10. Write a program to print a table of 5.
+
+   /*#include<stdio.h>
+   int main()
+   {
+       int i;
+       for(i=1;i<=10;i++)
+       {
+           printf("5 * %d=%d\n",i,5*i);
+       }
+       return 0;
+   }*/
+
